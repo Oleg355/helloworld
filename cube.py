4bbca755bdbee06ae8ae2000a3087d223dcf0c36
Oleg355 <58139185+Oleg355@users.noreply.github.com>
2019-11-24 16:50:28 +0300

    Add files via upload

---
+++ cube.py
@@ -0,0 +1,11 @@
+def name():    
+    print("Нужно возвести в куб что-то еще?")
+    x = int(input())
+    print(x*x)
+
+print("Привет! Это программа котрая возводит число в куб.")
+print("Введи число.")
+x = int(input())
+print(x*x*x)
+while True:
+    name()
