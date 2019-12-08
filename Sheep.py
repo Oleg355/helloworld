a3f62b1bf31e85ae86892ab8d30a991fd7bdbd19
Oleg355 <58139185+Oleg355@users.noreply.github.com>
2019-12-08 17:20:41 +0300

    Add files via upload

---
+++ Sheep.py
@@ -0,0 +1,15 @@
+import mcpi.minecraft as minecraft
+mc = minecraft.Minecraft.create()
+import keyboard
+def chikibamboni():
+    mc.setBlocks(pos.x+2,pos.y,pos.z-2,pos.x+3,pos.y+5,pos.z-1,35)
+    mc.setBlocks(pos.x+2,pos.y,pos.z+2,pos.x+3,pos.y+5,pos.z+1,35)
+    mc.setBlocks(pos.x-2,pos.y,pos.z-2,pos.x-3,pos.y+5,pos.z-1,35)
+    mc.setBlocks(pos.x-2,pos.y,pos.z+2,pos.x-3,pos.y+5,pos.z+1,35)
+    mc.setBlocks(pos.x+3,pos.y+5,pos.z+2,pos.x-3,pos.y+9,pos.z-2,35)
+    mc.setBlocks(pos.x+2,pos.y+6,pos.z+2,pos.x+4,pos.y+8,pos.z-2,35)
+    mc.postToChat("1")
+while True:
+    pos = mc.player.getTilePos()
+    if keyboard.is_pressed('c'):
+        chikibamboni()
