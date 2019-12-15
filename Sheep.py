e3a9094ed9ace18fbcad66302abcf28fc335012e
Oleg355 <58139185+Oleg355@users.noreply.github.com>
2019-12-15 09:26:57 +0300

    Add files via upload

--- Sheep.py
+++ Sheep.py
@@ -7,7 +7,7 @@ def chikibamboni():
     mc.setBlocks(pos.x-2,pos.y,pos.z-2,pos.x-3,pos.y+5,pos.z-1,35)
     mc.setBlocks(pos.x-2,pos.y,pos.z+2,pos.x-3,pos.y+5,pos.z+1,35)
     mc.setBlocks(pos.x+3,pos.y+5,pos.z+2,pos.x-3,pos.y+9,pos.z-2,35)
-    mc.setBlocks(pos.x+2,pos.y+6,pos.z+2,pos.x+4,pos.y+8,pos.z-2,35)
+    mc.setBlocks(pos.x+3,pos.y+9,pos.z+1,pos.x+5,pos.y+11,pos.z-1,35)
     mc.postToChat("1")
 while True:
     pos = mc.player.getTilePos()
